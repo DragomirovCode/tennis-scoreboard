2c7660d6e8f0ba972c4efeadc345b2a5fe18edee
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-25 18:45:49 +0300

    chore: Перенёс логику в "MatchesService"

--- src/main/java/ru/dragomirov/dto/CreateMatchesDTOFactory.java
+++ src/main/java/ru/dragomirov/dto/CreateMatchesDTOFactory.java
@@ -1,35 +1,11 @@
 package ru.dragomirov.dto;
 
 public class CreateMatchesDTOFactory {
-
     public MatchesDTO createMatches(PlayersDTO player1, PlayersDTO player2, String score) {
         MatchesDTO matches = new MatchesDTO();
         matches.setPlayer1(player1);
         matches.setPlayer2(player2);
         matches.setScore(score);
         return matches;
-    }
-
-    public void addPointsToPlayers(PlayersDTO player) {
-        int currentScore = player.getScore();
-        int additionalPoints = 0;
-
-        switch (currentScore) {
-            case 0:
-                additionalPoints = 15;
-                break;
-            case 15:
-                additionalPoints = 30;
-                break;
-            case 30:
-                additionalPoints = 40;
-                break;
-        }
-
-        player.setScore(additionalPoints);
-    }
-
-    public int getPlayerScore(PlayersDTO player) {
-        return player.getScore();
     }
 }
