77114d61544f05a599d5f8a2b89b2f6c461afc3b
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-28 11:05:24 +0300

    feat: Добавил "playersDAO" и "matchesDAO"

--- src/main/java/ru/dragomirov/services/PlayersService.java
+++ src/main/java/ru/dragomirov/services/PlayersService.java
@@ -1,4 +1,14 @@
 package ru.dragomirov.services;
 
+import ru.dragomirov.dao.MatchesDAO;
+import ru.dragomirov.dao.PlayersDAO;
+
 public class PlayersService {
+    private final MatchesDAO matchesDAO;
+    private final PlayersDAO playersDAO;
+
+    public PlayersService(MatchesDAO matchesDAO, PlayersDAO playersDAO) {
+        this.matchesDAO = matchesDAO;
+        this.playersDAO = playersDAO;
+    }
 }
