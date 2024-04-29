3632000e5855a631f3e0c79d54516be93b794fed
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-04-29 17:27:23 +0300

    feat: Добавил "doGet" методд

--- src/main/java/ru/dragomirov/servlets/MatchHistoryServlet.java
+++ src/main/java/ru/dragomirov/servlets/MatchHistoryServlet.java
@@ -1,6 +1,24 @@
 package ru.dragomirov.servlets;
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.dragomirov.commons.BaseServlet;
 
-public class MatchHistoryServlet {
+import java.io.IOException;
+
+@WebServlet(name = "MatchHistoryServlet", urlPatterns = "/matches")
+public class MatchHistoryServlet extends BaseServlet {
+    @Override
+    public void init() {}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            // Передаем управление JSP-странице
+            req.getRequestDispatcher("/matches.jsp").forward(req, resp);
+        } catch (Exception e) {
+            http500Errors(resp, e, "Ошибка при загрузке страницы.");
+        }
+    }
 }
