8cb0981fce92cf940b2b4b1f008db14a3483d86d
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-02 18:54:22 +0300

    chore: Убрал лишние отступы

--- src/main/java/ru/dragomirov/dao/CrudDAO.java
+++ src/main/java/ru/dragomirov/dao/CrudDAO.java
@@ -5,12 +5,8 @@ import java.util.Optional;
 
 public interface CrudDAO<T, ID> {
     Optional<T> findById(ID id);
-
     List<T> findAll();
-
     void save(T entity);
-
     Optional<T> update(T entity);
-
     void delete(ID id);
 }
