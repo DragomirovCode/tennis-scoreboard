8870c2a9a2c4090e652e45e0123c0c9934b0b46e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-02 22:22:05 +0300

    chore: Удалил ненужные переменные

--- src/main/java/ru/dragomirov/services/PlayersService.java
+++ src/main/java/ru/dragomirov/services/PlayersService.java
@@ -1,19 +1,13 @@
 package ru.dragomirov.services;
 
 import org.modelmapper.ModelMapper;
-import ru.dragomirov.dao.HibernateMatchesDAO;
-import ru.dragomirov.dao.HibernatePlayersDAO;
 import ru.dragomirov.dto.PlayersDTO;
 import ru.dragomirov.entities.Players;
 
 public class PlayersService {
-    private final HibernateMatchesDAO hibernateMatchesDAO;
-    private final HibernatePlayersDAO hibernatePlayersDAO;
     private final ModelMapper modelMapper;
 
     public PlayersService() {
-        this.hibernateMatchesDAO = new HibernateMatchesDAO();
-        this.hibernatePlayersDAO = new HibernatePlayersDAO();
         this.modelMapper = new ModelMapper();
     }
 
