07039f149833f103c0bd2095123cfb8b9c1748da
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-02 22:19:04 +0300

    feat: Добавил "modelMapper"

--- src/main/java/ru/dragomirov/services/PlayersService.java
+++ src/main/java/ru/dragomirov/services/PlayersService.java
@@ -1,5 +1,6 @@
 package ru.dragomirov.services;
 
+import org.modelmapper.ModelMapper;
 import ru.dragomirov.dao.HibernateMatchesDAO;
 import ru.dragomirov.dao.HibernatePlayersDAO;
 import ru.dragomirov.dto.PlayersDTO;
@@ -8,23 +9,19 @@ import ru.dragomirov.entities.Players;
 public class PlayersService {
     private final HibernateMatchesDAO hibernateMatchesDAO;
     private final HibernatePlayersDAO hibernatePlayersDAO;
+    private final ModelMapper modelMapper;
 
     public PlayersService() {
         this.hibernateMatchesDAO = new HibernateMatchesDAO();
         this.hibernatePlayersDAO = new HibernatePlayersDAO();
+        this.modelMapper = new ModelMapper();
     }
 
     public PlayersDTO toDTO(Players player) {
-        PlayersDTO dto = new PlayersDTO();
-        dto.setId(player.getId());
-        dto.setName(player.getName());
-        return dto;
+        return modelMapper.map(player, PlayersDTO.class);
     }
 
     public Players toEntity(PlayersDTO dto) {
-        Players player = new Players();
-        player.setId(dto.getId());
-        player.setName(dto.getName());
-        return player;
+        return modelMapper.map(dto, Players.class);
     }
 }
