597a961ff21eea673ae8164404bb393efefd4b10
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-02 19:45:52 +0300

    feat: Добавил переход на шаблон с ошибкой

--- src/main/java/ru/dragomirov/servlets/MainPageServlet.java
+++ src/main/java/ru/dragomirov/servlets/MainPageServlet.java
@@ -1,24 +1,27 @@
 package ru.dragomirov.servlets;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ru.dragomirov.commons.BaseServlet;
 
 import java.io.IOException;
 
 @WebServlet(name = "MainPageServlet", urlPatterns = "/")
-public class MainPageServlet extends BaseServlet {
+public class MainPageServlet extends HttpServlet {
     @Override
     public void init() {}
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         try {
             // Передаем управление JSP-странице
             req.getRequestDispatcher("/main.jsp").forward(req, resp);
         } catch (Exception e) {
-            http500Errors(resp, e, "Ошибка при загрузке страницы.");
+            // Перенаправляем на страницу ошибки
+            req.setAttribute("errorMessage", "Ошибка при загрузке страницы.");
+            req.getRequestDispatcher("/error.jsp").forward(req, resp);
         }
     }
 }
