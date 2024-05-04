7934adc6cb998bdf526e11a8c8c3b852147b8ab1
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-05-04 18:44:12 +0300

    feat: Добавил метод "shouldContinueGameAfterDeuce()", который проверяет, что если игрок 1 выигрывает очко при счёте 40-40, гейм не заканчивается

--- src/test/java/MatchScoreCalculationServiceTest.java
+++ src/test/java/MatchScoreCalculationServiceTest.java
@@ -1,2 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.dragomirov.dto.MatchesDTO;
+import ru.dragomirov.dto.MatchesDTOFactory;
+import ru.dragomirov.dto.PlayersDTO;
+import ru.dragomirov.dto.PlayersDTOFactory;
+import ru.dragomirov.services.MatchScoreCalculationService;
+
 public class MatchScoreCalculationServiceTest {
+    @Test
+    public void shouldContinueGameAfterDeuce(){
+        PlayersDTOFactory playersDTOFactory = new PlayersDTOFactory();
+        PlayersDTO player = playersDTOFactory.createPlayers("player", "40");
+        PlayersDTO opponent = playersDTOFactory.createPlayers("opponent", "40");
+
+        MatchesDTOFactory matchesDTOFactory = new MatchesDTOFactory();
+        MatchesDTO matches = matchesDTOFactory.createMatches(player, opponent);
+
+        MatchScoreCalculationService service = new MatchScoreCalculationService();
+        service.addPointsToPlayers(matches, player, opponent);
+
+        Assertions.assertEquals("AD", player.getScore());
+    }
 }
